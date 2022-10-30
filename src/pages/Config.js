1d5acdc75b7aeeadb1989ab0c1a4d04bc6ec3918
Frank Oelschlägel <diafo@t-online.de>
2022-10-30 11:54:20 +0100

    canvas added and draw on it

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 export default function Config() {
+  const canvasRef = useRef(null);
+
   const [gateWidth, setGateWidth] = useState("2500");
   const [gateHeight, setGateHeight] = useState("2000");
   const [radius, setRadius] = useState("30");
@@ -12,8 +14,11 @@ export default function Config() {
     })
   );
 
+  let ctx = null;
+  let canv = null;
+
   useEffect(() => {
-    if (isNaN(gateHeight) || isNaN(gateWidth)) {
+    if (isNaN(gateHeight) || isNaN(gateWidth) || isNaN(radius)) {
       setQm("--");
     } else {
       setQm(
@@ -22,11 +27,74 @@ export default function Config() {
           minimumFractionDigits: 2,
         })
       );
+      show();
     }
-  }, [gateHeight, gateWidth]);
+  }, [gateHeight, gateWidth, radius]);
+
+  function show() {
+    canv = canvasRef.current;
+    //console.log(canv);
+    ctx = canv.getContext("2d");
+    //console.log(ctx);
+
+    let canvBreite = 6000;
+    let startY = 3400;
+    let startXTemp = 0;
+
+    let torBreite = +gateWidth;
+    let torHoehe = +gateHeight;
+
+    let torBreitePrev = torBreite;
+    let torHoehePrev = torHoehe;
+    let radiusPrev = +radius;
+
+    canvBreite = canv.width;
+    console.log(canvBreite);
+    startXTemp = (canvBreite - torBreitePrev) / 2;
+    console.log(torBreitePrev);
+    console.log(startXTemp);
+
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    ctx.lineWidth = 40;
+
+    ctx.beginPath();
+    ctx.moveTo(startXTemp, startY);
+    ctx.lineTo(startXTemp, startY - torHoehePrev + radiusPrev);
+    ctx.arcTo(
+      startXTemp,
+      startY - torHoehePrev,
+      startXTemp + radiusPrev,
+      startY - torHoehePrev,
+      radiusPrev
+    );
+    ctx.lineTo(startXTemp + torBreitePrev - radiusPrev, startY - torHoehePrev);
+    ctx.arcTo(
+      startXTemp + torBreitePrev,
+      startY - torHoehePrev,
+      startXTemp + torBreitePrev,
+      startY - torHoehePrev + radiusPrev,
+      radiusPrev
+    );
+    ctx.lineTo(startXTemp + torBreitePrev, startY);
+    ctx.closePath();
+    ctx.stroke(); // Draw it
+
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.moveTo(0, startY);
+    ctx.lineTo(canvBreite, startY);
+    ctx.stroke(); // Draw it
+
+    //setInterval(myTimer, 4);
+    //myTimer(ctx, canv);
+  }
 
   return (
     <>
+      <StyledCanvas id="canvas" ref={canvasRef} width={6000} height={4000}>
+        Your browser does not support the HTML5 canvas tag.
+      </StyledCanvas>
+
       <StyledH3>Sichtbare Torfläche: {qm} qm</StyledH3>
 
       <label htmlFor="gateW">Toröffnung-Breite in mm</label>
@@ -68,7 +136,7 @@ export default function Config() {
       ></StyledInput>
       {isNaN(radius) ? (
         <StyledMessage>Bitte nur Zahlen eingeben!</StyledMessage>
-      ) : +radius < 0 || +radius > gateWidth / 2 ? (
+      ) : +radius < 0 || +radius > gateWidth / 2 || +radius > gateHeight ? (
         <StyledMessage>
           Zulässige Werte für Radius: 0 bis 50% der Breite
         </StyledMessage>
@@ -76,6 +144,12 @@ export default function Config() {
     </>
   );
 }
+
+const StyledCanvas = styled.canvas`
+  border: 1px solid #d3d3d3;
+  margin: 10px auto;
+  width: 80%;
+`;
 
 const StyledH3 = styled.h3`
   margin: 10px;
