4e8055b8d3ff17c3892f0f6b09671216faf71738
Frank Oelschlägel <diafo@t-online.de>
2022-10-30 10:57:20 +0100

    radius-input added

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 export default function Config() {
   const [gateWidth, setGateWidth] = useState("2500");
   const [gateHeight, setGateHeight] = useState("2000");
+  const [radius, setRadius] = useState("30");
   const [qm, setQm] = useState(
     ((+gateWidth * +gateHeight) / 1000000).toLocaleString(undefined, {
       maximumFractionDigits: 2,
@@ -27,8 +28,10 @@ export default function Config() {
   return (
     <>
       <StyledH3>Sichtbare Torfläche: {qm} qm</StyledH3>
+
       <label htmlFor="gateW">Toröffnung-Breite in mm</label>
       <StyledInput
+        id="gateW"
         type="text"
         onChange={(event) => setGateWidth(event.target.value)}
         value={gateWidth}
@@ -40,8 +43,10 @@ export default function Config() {
           Zulässige Werte für Höhe: 1000 bis 5000 mm
         </StyledMessage>
       ) : null}
+
       <label htmlFor="gateH">Toröffnung-Höhe in mm</label>
       <StyledInput
+        id="gateH"
         type="text"
         onChange={(event) => setGateHeight(event.target.value)}
         value={gateHeight}
@@ -51,6 +56,21 @@ export default function Config() {
       ) : +gateHeight < 1800 || +gateHeight > 3000 ? (
         <StyledMessage>
           Zulässige Werte für Höhe: 1800 bis 3000 mm
+        </StyledMessage>
+      ) : null}
+
+      <label htmlFor="radius">Torbogen-Radius in mm</label>
+      <StyledInput
+        id="radius"
+        type="text"
+        onChange={(event) => setRadius(event.target.value)}
+        value={radius}
+      ></StyledInput>
+      {isNaN(radius) ? (
+        <StyledMessage>Bitte nur Zahlen eingeben!</StyledMessage>
+      ) : +radius < 0 || +radius > gateWidth / 2 ? (
+        <StyledMessage>
+          Zulässige Werte für Radius: 0 bis 50% der Breite
         </StyledMessage>
       ) : null}
     </>
